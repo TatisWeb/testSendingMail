f99ef49840f17d3990a91a9bd56faaeaee11c9ac
Sergey Tarsenko <67330524+TatisWeb@users.noreply.github.com>
2020-11-19 20:44:39 +0400

    Update sendmail.php

--- sendmail.php
+++ sendmail.php
@@ -10,17 +10,18 @@
 	$mail->setLanguage('ru', 'phpmailer/language/');
 	$mail->IsHTML(true);
      
-        $mail->Host       = 'smtp.yandex.ru'; // SMTP сервера вашей почты
-        $mail->Username   = 'sergeytatis@yandex.ru'; // Логин на почте
-        $mail->Password   = 'matrix3d'; // Пароль на почте
+        $mail->Host       = 'smtp.mail.ru'; // SMTP сервера вашей почты
+        $mail->Username   = 'sergeytatis@mail.ru'; // Логин на почте
+        $mail->Password   = 'million777'; // Пароль на почте
         $mail->SMTPSecure = 'ssl';
         $mail->Port       = 465;
 	//От кого письмо
-	$mail->setFrom('sergeytatis@yandex.ru', 'Фрилансер по жизни');
+	$mail->setFrom('sergeytatis@mail.ru', 'Фрилансер по жизни');
 	//Кому отправить
-	$mail->addAddress('sergeytatis@gmail.com);
+	$mail->addAddress('sergeytatis@gmail.com');
+	$mail->addAddress('sergeytatis@yandex.ru');
 	//Тема письма
-	$mail->Subject = 'Привет! Это "Фрилансер по жизни"';
+	$mail->Subject = 'Привет! Это Фрилансер по жизни';
 
 	//Рука
 	$hand = "Правая";
